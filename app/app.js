02d2321771d00cb74ad51688b469bc74e1f414e5
unknown <chtml@mail.com>
2015-08-09 15:41:32 +0800

    odify

---
+++ app/app.js
@@ -0,0 +1,31 @@
+window.qianXun = angular.module('qianXun', ['ionic', 'DelegateEvents']);
+
+qianXun.config(function ($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+
+        .state('qianxun', { //父级路由---所有自路由都在qianxun下
+            abstract: true, // 为子状态提供一个 base url，其下所有子状态的 url 都是相对父状态的
+            url: '/qianxun',
+            templateUrl: 'source/tp/main.html'
+        })
+        .state('qianxun.shop-list', { //门店列表页
+            url: '/shop-list.htm',
+            templateUrl: 'source/tp/shop_list.html',
+            controller: 'shopListCtrl'
+        });
+
+    // $urlRouterProvider.when(' ', '/qianxun/shop-list.htm');
+
+    $urlRouterProvider.otherwise('/qianxun/shop-list.htm');
+
+    // 处理在状态配置中指定的路由之外的 url 请求
+    // var isShow = localStorage.getItem('qianxun_START');
+
+    // if (isShow) {
+    //     $urlRouterProvider.otherwise('/forum/start.htm');
+    // } else {
+    //     $urlRouterProvider.otherwise('/forum/start.htm');
+    // }
+
+});
